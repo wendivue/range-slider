76780113519f1c4d04d9af5707e7dfdc72d248a2
wendivue <vovas2345@gmail.com>
2020-10-02 21:35:40 +1000

    add interface & utils

---
+++ src/ts/interface.ts
@@ -0,0 +1,9 @@
+interface Config {
+  min: number;
+  max: number;
+  type: string;
+  input: boolean;
+  range: boolean;
+}
+
+export { Config };
---
+++ src/ts/utils.ts
@@ -0,0 +1,6 @@
+const FROM = 'from';
+const TO = 'to';
+const INPUTFROM = 'inputFrom';
+const INPUTTO = 'inputTo';
+
+export { FROM, TO, INPUTFROM, INPUTTO };
