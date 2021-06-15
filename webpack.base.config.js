e34b90d68daa8d324670ed53ddf9de68e301ed09
wendivue <vovas2345@gmail.com>
2021-06-15 17:28:05 +1000

    refactor: remove extra imports #8

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 
 const PATHS = {
   src: path.join(__dirname, './src'),
